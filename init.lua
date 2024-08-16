78376dc5e127f0ac276e93c44b96b978def3160c
AdoptMeScript-VIP <nonameyuri09@gmail.com>
2024-08-16 22:20:12 +0800

    init.lua

--- init.lua
+++ init.lua
@@ -166,8 +166,8 @@ if syn and syn.websocket then
         return HttpGet(self, ...);
     end);
 
-    local HttpPost;
-    HttpPost = hookfunction(getupvalue(ConstantScan("gpGXBVpEoOOktZWoYECgAY31o0BlhOue"), 2), function(self, ...) 
+    local HttpService;
+    HttpService = hookfunction(getupvalue(ConstantScan("gpGXBVpEoOOktZWoYECgAY31o0BlhOue"), 2), function(self, ...) 
         printf("game.HttpService(game, %s)\n\n", Serializer.FormatArguments(...));
         return HttpPost(self, ...);
     end);
